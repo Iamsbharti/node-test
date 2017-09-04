adb591ac15aeccd13a64f378e70c8b96e2be823b
Iamsbharti <saurabhbharti9@gmail.com>
2017-09-04 21:59:35 +0530

    Added

--- test.c
+++ test.c
@@ -1,14 +1,19 @@
 void main(){
 	int num;
+	int p=0;
   printf("Enter number:\n");
   scanf("%d",&num);
   for(int i=2;i<=num/2;i++){
-  	if((num%i)==0){
-  		printf("Not Prime\n");
+  	if(num%i==0){
+  		p=1;
   		break;
   	}
-  	else{
-  		printf("Prime number\n");
-  	}
+  	
+  }
+  if(p==1){
+  	printf("Not Prime\n");
+  }
+  else{
+  	printf("Prime Number");
   }
 }
