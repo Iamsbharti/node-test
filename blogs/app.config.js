b6fe1b69b3257cc578c318d9173a7ccea3e7c056
saurabh <saurabhbharti9@gmail.com>
2020-06-26 02:21:01 +0530

    removed

--- blogs/app.config.js
+++
@@ -1,9 +0,0 @@
-let config = {};
-config.port = 3000;
-config.allowedCorsOrigin = "*";
-config.env = "dev";
-config.db = {
-  url: "mongodb://127.0.0.1:27017/blogs",
-};
-config.apiVersion = "api/v1";
-module.exports = config;
--- blogs/routes/blogRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-//routes
-const blogController = require("../controllers/blogControllers");
-router.get("/createBlog", blogController.createBlog);
-router.get("/getBlogById/:id", blogController.getBlogById);
-module.exports = router;
