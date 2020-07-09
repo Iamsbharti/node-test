842ae501f027621664d1c470538c97808eaa42cb
saurabh <saurabhbharti9@gmail.com>
2020-07-09 10:09:57 +0530

    file sync handler

--- tests/fileHandler.js
+++ tests/fileHandler.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
 
 let fileReader = fs.readFileSync("test.c");
-console.log("Reading file sync");
+console.log("Reading file sync-buffer");
 console.log(fileReader);
+
+let fileReaderEncoded = fs.readFileSync("test.c", "utf-8");
+console.log("Reading file sync-encoded");
+console.log(fileReaderEncoded);
