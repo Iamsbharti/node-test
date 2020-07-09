9741308590a77b6cccbe2b07db6de55f6ba4d1ae
saurabh <saurabhbharti9@gmail.com>
2020-07-09 10:25:58 +0530

    file async handler-cb

--- tests/fileHandler.js
+++ tests/fileHandler.js
@@ -13,10 +13,20 @@ console.log(fileReaderEncoded);
 /**Read file  async (wrong implementation of async as sync method)
  * gives error cb not defined ERR_INVALID_CALLBACK
  */
-console.log("Reading File");
-let fileReadAsyncError = fs.readFile("test.py");
-console.log("Reading File finished");
-console.log("File.", fileReadAsyncError);
+//console.log("Reading File");
+//let fileReadAsyncError = fs.readFile("test.py");
+//console.log("Reading File finished");
+//console.log("File.", fileReadAsyncError);
 
 /**Read file  async correct implementation*/
 console.log("read file async");
+fs.readFile("test.py", "utf-8", (error, file) => {
+  if (error) {
+    console.log("Error in reading file async");
+  } else {
+    console.log("File Read Async", file);
+  }
+});
+console.log("File reading done");
+
+console.log("Random ops", Math.random());
